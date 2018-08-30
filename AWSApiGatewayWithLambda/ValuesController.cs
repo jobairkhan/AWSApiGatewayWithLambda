983a18e008119a24cc2fcb100c6bd29740de2a78
Jobair <m.j.khan.bd@gmail.com>
2018-08-30 05:55:16 +0100

    Building

---
+++ AWSApiGatewayWithLambda/ValuesController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AWSApiGatewayWithLambda {
+    [Route("api/[controller]")]
+    public class ValuesController : Controller {
+
+        private readonly ILogger<ValuesController> _logger;
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get() {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id) {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value) {
+            _logger.LogInformation($"Post Value {value}");
+        }
+    }
+}
